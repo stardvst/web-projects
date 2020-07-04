bce148767646a1a2dee5952fba4cf397dc3f373a
Lilia <lilia.manukyan@hotmail.com>
2020-07-04 12:34:54 +0400

    js part 2 (events)

--- test-site/scripts/main.js
+++ test-site/scripts/main.js
@@ -1,2 +1,38 @@
-const theHeading = document.querySelector('h1');
+const theHeading = document.querySelector('h2');
 theHeading.textContent = 'Hello world!';
+
+document.querySelector('img').onclick = function () {
+    alert('Ouch! Stop poking me!');
+};
+
+let image = document.querySelector('img');
+image.onclick = function () {
+    let theSrc = image.getAttribute('src');
+    if (theSrc === 'images/firefox-icon.png') {
+        image.setAttribute('src', 'images/firefox2.png');
+    } else {
+        image.setAttribute('src', 'images/firefox-icon.png');
+    }
+};
+
+// button click event
+function setUserName() {
+    let name = prompt('Please enter your name.');
+    if (!name || name === null) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', name);
+        theHeading.textContent = 'Mozilla is cool, ' + name;
+    }
+}
+
+let storedName = localStorage.getItem('name');
+if (!storedName) {
+    setUserName();
+} else {
+    theHeading.textContent = 'Mozilla is cool, ' + storedName;
+}
+
+document.querySelector('button').onclick = function () {
+    setUserName();
+};
