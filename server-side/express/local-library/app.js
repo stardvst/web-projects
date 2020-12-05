eca6b23c8ef9edc5c7c04196b9f21d0aed4de2a3
Lilia <lilia.manukyan@hotmail.com>
2020-12-05 16:52:53 +0400

    setup mongoose, create models

--- server-side/express/local-library/app.js
+++ server-side/express/local-library/app.js
@@ -9,6 +9,13 @@ var usersRouter = require('./routes/users');
 
 var app = express();
 
+var mongoose = require('mongoose');
+var mongoDB =
+  "mongodb+srv://root:root@cluster0.sv0vg.mongodb.net/local_library?retryWrites=true&w=majority";
+mongoose.connect(mongoDB, { useNewUrlParser: true });
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
